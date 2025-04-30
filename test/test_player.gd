93eb79ad697981616606af820ab73ae5637ee860
Fabien MARCUCCINI <fabien.marcuccini@proton.me>
2025-04-30 19:23:02 +0200

    refactor and comment test_player.gd

--- test/test_player.gd
+++ test/test_player.gd
@@ -1,21 +1,25 @@
 extends GutTest
 
+## Preloaded player scene for instantiating in tests.
+## Reference to the player node in the instantiated scene.
 var player_scene = preload("res://scenes/player_scene.tscn") 
 var player_node
 
-
+## Sets up the player scene and node before each test case.
+## This function instantiates the player scene and sets up the player node reference.
 func before_each():
 	var instance = player_scene.instantiate()
 	add_child(instance)
 	await get_tree().process_frame
-
 	player_node = instance.get_node("Player")
 
-
+## Cleans up the player node after each test case.
+## This function frees the player node after each test case execution.
 func after_each():
 	player_node.queue_free()
 
-
+## Tests player movement when moving forward.
+## Simulates pressing the "move_forward" input action and checks that the player moves forward.
 func test_move_forward():
 	Input.action_press("move_forward")
 	await get_tree().process_frame
@@ -32,7 +36,8 @@ func test_move_forward():
 
 	Input.action_release("move_forward")
 
-
+## Tests player movement when moving backward.
+## Simulates pressing the "move_back" input action and checks that the player moves backward.
 func test_move_back():
 	Input.action_press("move_back")
 	await get_tree().process_frame
@@ -49,7 +54,8 @@ func test_move_back():
 
 	Input.action_release("move_back")
 
-
+## Tests player movement when moving left.
+## Simulates pressing the "move_left" input action and checks that the player moves left.
 func test_move_left():
 	Input.action_press("move_left")
 	await get_tree().process_frame
@@ -66,7 +72,8 @@ func test_move_left():
 
 	Input.action_release("move_left")
 
-
+## Tests player movement when moving right.
+## Simulates pressing the "move_right" input action and checks that the player moves right.
 func test_move_right():
 	Input.action_press("move_right")
 	await get_tree().process_frame
@@ -83,7 +90,8 @@ func test_move_right():
 
 	Input.action_release("move_right")
 
-
+## Tests player sprint functionality.
+## Simulates pressing the "move_forward" and "sprint" input actions and checks that the player accelerates.
 func test_sprint():
 	Input.action_press("move_forward")
 	Input.action_press("sprint")
@@ -103,7 +111,8 @@ func test_sprint():
 	Input.action_release("sprint")
 	Input.action_release("move_forward")
 	
-
+## Tests player jump functionality.
+## Simulates pressing the "jump" input action and checks that the player jumps upwards.
 func test_jump():
 	var min_position_y = player_node.global_position.y
 	var max_position_y = min_position_y
