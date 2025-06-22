cd0b278ee50d8e172ff7d2c97b794854b4272dfd
Salim-amd <salim.amd13@gmail.com>
2025-06-22 21:32:03 +0200

    add func _on_finish_line_entered(body) script race_manager

--- lfs/scripts/race_manager.gd
+++ lfs/scripts/race_manager.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var player_car = $PlayerCar/Car_010
+
 var race_started := false
 var race_time := 0.0
 
@@ -14,3 +16,8 @@ func start_race():
 	race_started = true
 	race_time = 0.0
 	print("Course démarrée")
+
+func _on_finish_line_entered(body):
+	if body == player_car and race_started:
+		race_started = false
+		print("Temps final: %.2f secondes" % race_time)
