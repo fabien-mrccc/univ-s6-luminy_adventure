3ae4fd2f0a83a91ca737f74a8123f810a2a845a6
Salim-amd <salim.amd13@gmail.com>
2025-06-22 21:31:06 +0200

    add func _physics_process() script race_manager

--- lfs/scripts/race_manager.gd
+++ lfs/scripts/race_manager.gd
@@ -3,6 +3,13 @@ extends Node3D
 var race_started := false
 var race_time := 0.0
 
+func _physics_process(delta: float) -> void:
+	if race_started:
+		race_time += delta
+		print("Temps: %.2f secondes" % race_time)
+	if Input.is_action_just_pressed("move_forward") and not race_started:
+		start_race()
+
 func start_race():
 	race_started = true
 	race_time = 0.0
