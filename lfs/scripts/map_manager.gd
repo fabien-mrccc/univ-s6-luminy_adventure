97a26f85c22996f9f9432fe7b9f11966126b480c
Salim-amd <salim.amd13@gmail.com>
2025-06-23 22:17:32 +0200

    add func _on_offroad_zone_exited(body) script map_manager

--- lfs/scripts/map_manager.gd
+++ lfs/scripts/map_manager.gd
@@ -19,3 +19,11 @@ func _on_finish_line_entered(body):
 	if body == player_car and race_started:
 		race_started = false
 		chrono_label.text = "Temps final : %.2f s" % race_time
+		
+func _on_offroad_zone_exited(body):
+	if body == player_car:
+		var z_pos = body.global_position.z
+		var respawn_pos = Vector3(0, 10, z_pos)
+		body.global_position = respawn_pos
+		body.linear_velocity = Vector3.ZERO
+		body.angular_velocity = Vector3.ZERO
