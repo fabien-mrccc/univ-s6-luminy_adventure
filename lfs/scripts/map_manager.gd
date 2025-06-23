f7eb020062a3fdada069ba939666c7bd8b863045
Salim-amd <salim.amd13@gmail.com>
2025-06-23 22:12:42 +0200

    add func _on_finish_line_entered(body) script map_manager

--- lfs/scripts/map_manager.gd
+++ lfs/scripts/map_manager.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var chrono_label = $CanvasLayer/ChronoLabel
+@onready var player_car = $PlayerCar/Car_010
 
 var race_started := false
 var race_time := 0.0
@@ -13,3 +14,8 @@ func _physics_process(delta: float) -> void:
 func start_race():
 	race_started = true
 	race_time = 0.0
+
+func _on_finish_line_entered(body):
+	if body == player_car and race_started:
+		race_started = false
+		chrono_label.text = "Temps final : %.2f s" % race_time
